15d43a28abdc505e6f6af5a59c9222ec5a430216
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2017-05-29 12:24:06 +0300

    fixed map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
  function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 45.747413, lng: 21.228927};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 15,
     center: uluru
   });
   var marker = new google.maps.Marker({
